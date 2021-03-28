fab9ee908ca29a880f5a644ce11c5f1ac5c027e3
Donald Kibet <chelashawdonald@yahoo.com>
2021-03-29 00:56:26 +0300

    repository restructure

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const Root: React.FC = () => {
-    return <div>
-        <h2>MTRH Cardiology Working ...</h2>
+  return (
+    <div>
+      <h2>MTRH Cardiology Working ...</h2>
     </div>
-}
+  );
+};
 
 export default Root;
